a08a43a17b7e893cb7fecfa45b19079f6477eb31
admin <oleksandrtsuprenko@gmail.com>
2025-11-06 21:47:54 +0100

    06.11.2025 commit

---
+++ array2D/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "banana", "peach"};
+        String[] colors = {"red", "green", "blue"};
+        String[] vegetables = {"cucumber", "potato"};
+
+         
+    }
+}
