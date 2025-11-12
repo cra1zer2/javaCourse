8430a271a3c69cdf6c551be3ccbfaf36d7c226ef
admin <oleksandrtsuprenko@gmail.com>
2025-11-12 09:15:47 +0100

    12.11.2025

--- array2D/src/Main.java
+++ array2D/src/Main.java
@@ -1,9 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        String[] fruits = {"apple", "banana", "peach"};
-        String[] colors = {"red", "green", "blue"};
-        String[] vegetables = {"cucumber", "potato"};
+//        String[] fruits = {"apple", "banana", "peach"};
+//        String[] colors = {"red", "green", "blue"};
+//        String[] vegetables = {"cucumber", "potato"};
+//
+//        String[][] groceries = {fruits, colors, vegetables};
+//        // or the same:
+//        String[][] groceries2 = {
+//                {"apple", "banana", "peach"},
+//                {"red", "green", "blue"},
+//                {"cucumber", "potato"}
+//
+//        };
+//        // to change value:
+//        //by id - row 1, column 0
+//        groceries2[2][0] = "coconut";
+//
+//
+//        for(String[] foods : groceries2){
+//            for(String food : foods){
+//                System.out.print(food + " ");
+//            }
+//            System.out.println();
+//        }
 
-         
+        char[][] telephone = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'},
+                {'*', '0', '#'}
+        };
+
+        for(char[] row : telephone){
+            for(char number : row){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
     }
 }
