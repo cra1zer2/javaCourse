2ad768e2ec74f93678f955a57507a0a25bfbd42c
admin <oleksandrtsuprenko@gmail.com>
2025-11-28 12:12:26 +0100

    OOP

--- objectOrientedProgramming/src/Main.java
+++ objectOrientedProgramming/src/Main.java
@@ -1,10 +1,5 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
 
         Car car = new Car();
 
@@ -33,8 +28,5 @@ public class Main {
         System.out.println(car.make + " " + car.model);
 
         System.out.println(car2.make + " " + car2.model);
-
-
-        sc.close();
     }
 }
