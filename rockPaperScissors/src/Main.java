f2a818245394f73ee1931f214b4a0bd233eb717e
cra1zer2 <oleksandrtsuprenko@gmail.com>
2025-11-30 16:56:15 +0100

    rps fix, worked on mac, wouldn't launch on windows without String args in main

--- rockPaperScissors/src/Main.java
+++ rockPaperScissors/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
 
