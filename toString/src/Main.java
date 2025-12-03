409d8567f158301b38ec9e9a7c58c15aab0a1984
cra1zer2 <oleksandrtsuprenko@gmail.com>
2025-12-03 14:07:05 +0100

    toString method

---
+++ toString/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Ford", "Mustang", 2020, "yellow");
+        Car car2 = new Car("Mercedes", "Benz", 2020, "red");
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+}
